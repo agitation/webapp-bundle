e01a7aab94ff6c7525f82838e24950944ae698f8
Alexander Günsche <mail@lxg.de>
2018-01-28 11:58:49 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+**Agitation** is an e-commerce framework, based on Symfony2, focussed on extendability through plugged-in APIs, UIs, payment modules and other components.
+
+## agitation/webapp-bundle
+
+This bundle helps creating modern web applications.
---
+++ lib/AgitWebappBundle.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+/*
+ * @package    agitation/webapp-bundle
+ * @link       http://github.com/agitation/webapp-bundle
+ * @author     Alexander Günsche
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\WebappBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AgitWebappBundle extends Bundle
+{
+}
